ef6915f977d44603d5f3e36346accdf5abed6df0
KunitcynSemyon <semen.kunicyn@gmail.com>
2020-11-27 18:49:43 +0400

    update

--- matrix_form.py
+++ matrix_form.py
@@ -1,78 +1,78 @@
-# from tkinter import *
-
-# master = Tk()
-# master.geometry("500x500")
-
-# def create_matrix(rows, columns):
-#     textMatrix = []
-#     for r in range(rows):
-#         textRow = []
-#         for c in range(columns):
-#             variable = StringVar()
-#             entry = Entry(master, textvariable=variable, width=2)
-#             entry.grid(row=r, column=c)
-#             textRow.append(variable)
-#         textMatrix.append(textRow)
-#     return textMatrix
-
-# n = IntVar()
-# m = IntVar()
-
-# n_label = Label(master, text = 'n:')
-# m_label = Label(master, text = 'm:')
-# n_label.pack()
-# m_label.pack()
-
-# row_entry = Entry(master)
-# column_entry = Entry(master)
-# row_entry.pack()
-# column_entry.pack()
-
-# #text_matrix = create_matrix(row_entry.get(), column_entry.get())
-
-# master.mainloop()
-
 import tkinter as tk
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.width = 500
-        self.height = 500
-        self.bg = 'red'
-        self.pack()
-        self.create_widgets()
+class SimpleTableInput(tk.Frame):
 
-    def create_widgets(self):
-        self.n_label = tk.Label(self)
-        self.n_label['text'] = 'n:'
-        self.n_label.pack(side = 'left')
+    def __init__(self, parent, rows, columns):
+        tk.Frame.__init__(self, parent)
+        self._entry = {}
+        self.rows = rows
+        self.columns = columns #register a command to use for validation 
+        vcmd = (self.register(self._validate), "%P") #create the table of widgets 
+        for row in range(self.rows):
+            for column in range(self.columns):
+                index = (row, column)
+                e = tk.Entry(self, validate="key", validatecommand=vcmd)
+                e.grid(row=row, column=column, stick="nsew")
+                self._entry[index] = e #adjust column weights so they all expand equally 
+        for column in range(self.columns):
+            self.grid_columnconfigure(column, weight=1) #designate a final, empty row to fill up any extra space 
+            self.grid_rowconfigure(rows, weight=1)
 
-        self.m_label = tk.Label(self)
-        self.m_label['text'] = 'm:'
-        self.m_label.pack(side = 'top')
+#Return a list of lists, containing the data in the table
+    def get(self):
+        result = []
+        for row in range(self.rows):
+            current_row = []
+            for column in range(self.columns):
+                index = (row, column)
+                current_row.append(self._entry[index].get())
+            result.append(current_row)
+        return result
 
-        self.row_entry = tk.Entry(self)
-        self.row_entry.pack()
+    def _validate(self, P):
+#Perform input validation. Allow only an empty value, or a value that can be converted to a float
+        if P.strip() == "":
+            return True
+        try:
+            f = float(P)
+        except ValueError:
+            self.bell()
+            return False
+        return True
 
-        self.column_entry = tk.Entry(self)
-        self.column_entry.pack()
+class Example(tk.Frame):
 
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Enter"
-        self.hi_there["command"] = self.create_matrix(self.row_entry.get(), self.column_entry.get())
-        self.hi_there.pack(side="top")
-        
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.table = SimpleTableInput(self, 3, 3)
+        self.submit = tk.Button(self, text="Submit", command=self.jordan(self.table.get(), 2, 3))
+        self.table.pack(side="top", fill="both", expand=True)
+        self.submit.pack(side="bottom")
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
+    def on_submit(self):
+        print(self.table.get())
 
-    def create_matrix(self, rows, column):
-        pass
+    def jordan(self, matrix, row, column):
+        row -= 1
+        column -= 1
+        temp_matrix = []
+        matrix = [[float(column) for column in row] for row in matrix]
+        for i in range(len(matrix)):
+            temp_matrix.append([])
+            for j in range(len(matrix[i])):
+                temp_val = (matrix[i][j] * matrix[row][column] - matrix[i][column] * matrix[row][j]) / matrix[row][column]
+                temp_matrix[i].append(temp_val)
+
+        for i in range(len(matrix)):
+            temp_matrix[i][column] = matrix[i][column] / matrix[row][column] * -1
+
+        for i in range(len(matrix[row])):
+            temp_matrix[row][i] = matrix[row][i] / matrix[row][column]
+
+        temp_matrix[row][column] = 1 / matrix[row][column]
+
+        return temp_matrix
 
 root = tk.Tk()
-root.geometry("500x500")
-app = Application(master=root)
-app.mainloop()
+Example(root).pack(side="top", fill="both", expand=True)
+root.mainloop()
